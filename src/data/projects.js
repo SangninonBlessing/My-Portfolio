1cb0aeb3239fdbb8246a748a84be6f0743d5013b
unknown <blessing.sangninon@gmail.com>
2026-07-08 23:17:58 +0000

    Correction de la photo de StudDiary

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,7 +7,7 @@ const projects = [
 
     status: "In Development",
 
-    image: {StudDiaryphoto},
+    image: StudDiaryphoto,
 
     description:
     "An all-in-one learning workspace currently in development, designed to help students and lifelong learners organize notes, manage courses, create quizzes, track their progress, and achieve their learning goals in one seamless experience. Future AI-powered features will deliver personalized insights, intelligent study assistance, and adaptive learning recommendations.",    
